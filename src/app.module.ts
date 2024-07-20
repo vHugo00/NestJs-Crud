b5c3e965b2c1dfa272ee98f7d21211ffb0e876d3
Vitor Hugo <vv001226@gmail.com>
2024-07-20 15:35:51 -0300

    criação de controller user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TempModule } from './temp/temp.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, TempModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
