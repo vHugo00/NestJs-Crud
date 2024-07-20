c1ac9650fc6803fb6e6dba0f74a3dbf2bc3b10ff
Vitor Hugo <vv001226@gmail.com>
2024-07-20 01:12:55 -0300

    iniciando e configurando projeto

---
+++ prisma/migrations/20240720035349_create_col_user_ativo/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "ativo" BOOLEAN NOT NULL DEFAULT true
+);
+INSERT INTO "new_User" ("email", "id", "name", "senha") SELECT "email", "id", "name", "senha" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
